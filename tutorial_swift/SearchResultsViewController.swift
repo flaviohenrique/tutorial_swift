b2a60ea319de9393a3252967fc9e29dbffbba333
Flavio Andrade <flavio.andrade@locaweb.com.br>
2015-07-08 19:58:37 -0300

    Part 3

--- tutorial_swift/SearchResultsViewController.swift
+++ tutorial_swift/SearchResultsViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
   
   var tableData = []
+  
+  let api = APIController()
   
   @IBOutlet weak var appsTableView : UITableView!
   
@@ -35,47 +37,17 @@ class SearchResultsViewController: UIViewController, UITableViewDataSource, UITa
     return cell
   }
   
-  func searchItunesFor(searchTerm: String) {
-    // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
-    let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-    
-    // Now escape anything else that isn't URL-friendly
-    if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
-      let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
-      let url = NSURL(string: urlPath)
-      let session = NSURLSession.sharedSession()
-      let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
-        println("Task completed")
-        if(error != nil) {
-          // If there is an error in the web request, print it to the console
-          println(error.localizedDescription)
-        }
-        var err: NSError?
-        if let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as? NSDictionary {
-          if(err != nil) {
-            // If there is an error parsing JSON, print it to the console
-            println("JSON Error \(err!.localizedDescription)")
-          }
-          if let results: NSArray = jsonResult["results"] as? NSArray {
-            dispatch_async(dispatch_get_main_queue(), {
-              self.tableData = results
-              self.appsTableView!.reloadData()
-            })
-          }
-        }
-      })
-      
-      // The task is just an object with all these properties set
-      // In order to actually make the web request, we need to "resume"
-      task.resume()
-    }
+  func didReceiveAPIResults(results: NSArray) {
+    dispatch_async(dispatch_get_main_queue(), {
+      self.tableData = results
+      self.appsTableView!.reloadData()
+    })
   }
-  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    searchItunesFor("JQ Software")
+    api.delegate = self
+    api.searchItunesFor("JQ Software")
     
     // Do any additional setup after loading the view, typically from a nib.
   }
